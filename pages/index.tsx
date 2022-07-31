4c12796476df8f51a3cf570d614739a1cccef22d
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-07-30 22:54:31 -0700

    4.3 Test Drive part Three

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-slate-400 py-20 px-10 grid gap-10'>
+    <div className='bg-slate-400 py-20 px-10 grid gap-10 min-h-screen'>
       <div className='bg-white p-6 rounded-3xl shadow-xl'>
         <span className='font-semibold text-3xl'>Select Item</span>
 
@@ -50,9 +50,37 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
+      <div className='bg-white p-10 rounded-2xl shadow-xl'>
+        <div className='flex mb-5 justify-between items-center'>
+          <span>⬅️</span>
+          <div>
+            <span className='space-x-5'>⭐️ 4.9</span>
+            <span className='shadow-xl p-2 rounded-md'>💖</span>
+          </div>
+        </div>
 
-      <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
+        <div className='bg-zinc-400 h-72 mb-5' />
+        <div className='flex flex-col'>
+          <span className='font-medium text-xl'>Swoon Launge</span>
+          <span className='text-xs text-gray-500'>Chair</span>
+          <div className='mt-3 mb-5 flex justify-between items-center'>
+            <div className='flex items-center'>
+              <input type="radio" />
+              <input type="radio" />
+              <input type="radio" />
+            </div>
+            <div className='flex items-center space-x-4'>
+              <button className='bg-blue-200 flex justify-center items-center aspect-square w-8 font-medium text-xl text-gray-500 shadow-xl rounded-xl'>-</button>
+              <span>1</span>
+              <button className='bg-blue-200 flex justify-center items-center aspect-square w-8 font-medium text-xl text-gray-500 shadow-xl rounded-xl'>+</button>
+            </div>
+          </div>
+          <div className='flex justify-between items-center'>
+            <span className='font-bold text-2xl'>$450</span>
+            <button className='bg-blue-500 text-center py-2 px-8 text-xs rounded-xl shadow-xl text-white cursor-pointer'>Add to cart</button>
+          </div>
+        </div>
+      </div>
     </div >
   );
 };
