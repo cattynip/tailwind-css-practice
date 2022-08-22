2e21c6ebfdc18b3a8b8905a3407d8a8174d62911
Craftzcat <craftzcat.ss@gmail.com>
2022-08-22 16:35:08 -0700

    4.10 Responsive Modifiers part Two

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className="grid min-h-screen gap-10 bg-slate-400 py-20 px-10">
+    <div className="grid min-h-screen gap-10 bg-slate-400 py-20 px-10 lg:grid-cols-2 xl:grid-cols-3 xl:place-content-center">
       <div className="flex flex-col rounded-3xl bg-white p-6 shadow-xl sm:bg-red-400 sm:hover:bg-pink-200 md:bg-teal-500 lg:bg-indigo-500 xl:bg-yellow-400 2xl:bg-pink-500">
         <span className="text-3xl font-semibold">Select Item</span>
 
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
       </div>
 
       <div className="group overflow-hidden rounded-2xl bg-white shadow-xl">
-        <div className="bg-blue-500 p-6 pb-14">
+        <div className="bg-blue-500 p-6 pb-14 xl:pb-40 portrait:bg-indigo-500 landscape:bg-teal-500">
           <span className="text-2xl text-white">Profile</span>
         </div>
         <div className="relative -top-5 rounded-3xl bg-white px-7 pt-9">
@@ -62,7 +62,7 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      <div className="rounded-2xl bg-white p-10 shadow-xl">
+      <div className="rounded-2xl bg-white p-10 shadow-xl lg:col-span-2 xl:col-span-1">
         <div className="mb-5 flex items-center justify-between">
           <span>⬅️</span>
           <div>
